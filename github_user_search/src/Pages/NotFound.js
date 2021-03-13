7c73f22a46925f2423b7aa0a7f9d66240b004084
Dipen parmar <dipensavji@gmail.com>
2021-03-13 11:02:21 +0530

    wip

---
+++ github_user_search/src/Pages/NotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const NotFound = () => {
+  return (
+    <div>
+      <h1>404! NotFound</h1>
+      <p className='lead'>The page you are looking for does not exist</p>
+    </div>
+  );
+};
