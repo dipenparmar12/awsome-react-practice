3be30b65273079b838b035a682c659eba4e2088d
Dipen Parmar <dipensavji@gmail.com>
2021-06-23 08:02:52 +0530

    Auth Done

--- jwt/src/App.js
+++ jwt/src/App.js
@@ -1,11 +1,20 @@
 import './App.css'
+import { Route, Switch } from 'react-router-dom'
 import Login from './component/Login'
+import HomeScreen from './component/HomeScreen'
+import RoutePrivate from './route/RoutePrivate'
+import useAuth from './hooks/UseAuth'
 
 function App() {
+  const { authLoading, authToken } = useAuth()
   return (
     <>
-      {/* <Test /> */}
-      <Login/>
+      <Switch>
+        {authToken && <Route exact path={'/'} component={HomeScreen} />}
+        {!authToken && <Route exact path={'/(|login)/'} component={Login} />}
+        {!authLoading && <RoutePrivate exact path={'/admin'} component={HomeScreen} />}
+        <Route component={() => <h1 className='center'> NOT FOUND </h1>} />
+      </Switch>
     </>
   )
 }
