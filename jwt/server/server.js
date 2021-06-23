cca2f4b2a80067461211665d8ddd02c170c612fa
Dipen Parmar <dipensavji@gmail.com>
2021-06-23 06:45:25 +0530

    success request

--- jwt/server/server.js
+++ jwt/server/server.js
@@ -4,23 +4,32 @@ const cors = require('cors')
 
 const app = express()
 
-var corsOptions = {
-  origin: 'http://localhost:8081',
-}
+// Add headers
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
-app.use(cors(corsOptions))
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-// parse requests of content-type - application/json
-app.use(bodyParser.json())
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+app.use(express.json({ limit: '100mb' }))
 
 // database
 const db = require('./app/models')
 const Role = db.role
 
-db.sequelize.sync()
+// db.sequelize.sync()
 // force: true will drop the table if it already exists
 // db.sequelize.sync({force: true}).then(() => {
 //   console.log('Drop and Resync Database with { force: true }');
@@ -42,7 +51,8 @@ require('./app/routes/auth.routes')(app)
 require('./app/routes/user.routes')(app)
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080
+// const PORT = process.env.PORT || 8080
+const PORT = 8001
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`)
 })
