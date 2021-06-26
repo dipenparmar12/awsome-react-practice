c1b1ca7ac2ccdf59ce2fcfec7920cd3e5b72de0b
Dipen Parmar <dipensavji@gmail.com>
2021-06-26 12:43:42 +0530

    Day 5, Flex Panel Gallery

--- js30days/src/App.js
+++ js30days/src/App.js
@@ -3,13 +3,22 @@ import styled from 'styled-components'
 import './App.css'
 import Day1DrumKit from './Day1DrumKit/Day1DrumKit'
 import Day2Clock from './Day2Clock/Day2Clock'
+import Day5Gallery from './Day5Gallery/Day5Gallery'
 
 const Ul = styled.ul`
   list-style: none;
   display: flex;
   box-shadow: 0 2px 30px 2px rgb(0 0 0 / 10%);
   margin: 0;
-  
+
+  li {
+    margin-right: 20px;
+  }
+
+  li::last-child {
+    margin-right: inherit;
+  }
+
   li a {
     display: inline-block;
     padding: 0.9em 0.2em;
@@ -35,14 +44,17 @@ function App() {
       <BrowserRouter>
         <div>
           <Ul>
-            <li style={{ marginRight: '20px' }}>
+            <li>
               <Link to='/day1-drumkit'>Day1 Drumkit</Link>
             </li>
-            <li style={{ marginRight: '20px' }}>
+            <li>
               <Link to='/day2-clock'>Day2 Clock</Link>
             </li>
-            <li style={{ marginRight: '20px' }}>
-              <Link to='/day3-'>Day3</Link>
+            <li>
+              <Link to='/day5-gallery'>Day5 gallery</Link>
+            </li>
+            <li>
+              <Link to='/'>Day3</Link>
             </li>
           </Ul>
         </div>
@@ -51,8 +63,13 @@ function App() {
           <Route exact path='/day1-drumkit'>
             <Day1DrumKit />
           </Route>
+
           <Route exact path='/day2-clock'>
             <Day2Clock />
+          </Route>
+
+          <Route exact path='/day5-gallery'>
+            <Day5Gallery />
           </Route>
 
           <Route exact path='*'>
