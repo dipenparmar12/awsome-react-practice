210b480c21b78b6bf5582eaf96ee07ad894a5b37
Dipen Parmar <dipensavji@gmail.com>
2021-06-26 12:54:07 +0530

    App routes object added

--- js30days/src/App.js
+++ js30days/src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
 import styled from 'styled-components'
-import './App.css'
 import Day1DrumKit from './Day1DrumKit/Day1DrumKit'
 import Day2Clock from './Day2Clock/Day2Clock'
 import Day5Gallery from './Day5Gallery/Day5Gallery'
@@ -38,43 +37,41 @@ const Ul = styled.ul`
     color: greenyellow;
   }
 `
+
+export const appRoutes = {
+  Day1DrumKit: {
+    name: 'Day1DrumKit',
+    path: `/Day1DrumKit`,
+    component: (props) => <Day1DrumKit {...props} />,
+  },
+  Day2Clock: {
+    name: 'Day2Clock',
+    path: `/Day2Clock`,
+    component: (props) => <Day2Clock {...props} />,
+  },
+  Day5Gallery: {
+    name: 'Day5Gallery',
+    path: `/Day5Gallery`,
+    component: (props) => <Day5Gallery {...props} />,
+  },
+}
+
 function App() {
   return (
     <>
       <BrowserRouter>
-        <div>
-          <Ul>
-            <li>
-              <Link to='/day1-drumkit'>Day1 Drumkit</Link>
+        <Ul>
+          {Object.values(appRoutes).map((route, i) => (
+            <li key={'r' + i}>
+              <Link to={route.path}>{route.name}</Link>
             </li>
-            <li>
-              <Link to='/day2-clock'>Day2 Clock</Link>
-            </li>
-            <li>
-              <Link to='/day5-gallery'>Day5 gallery</Link>
-            </li>
-            <li>
-              <Link to='/'>Day3</Link>
-            </li>
-          </Ul>
-        </div>
-
+          ))}
+        </Ul>
+        
         <Switch>
-          <Route exact path='/day1-drumkit'>
-            <Day1DrumKit />
-          </Route>
-
-          <Route exact path='/day2-clock'>
-            <Day2Clock />
-          </Route>
-
-          <Route exact path='/day5-gallery'>
-            <Day5Gallery />
-          </Route>
-
-          <Route exact path='*'>
-            <h1> Welcome to Javascript 30 day's challange</h1>
-          </Route>
+          {Object.values(appRoutes).map((route, i) => (
+            <Route key={'Route' + route.path} exact path={route.path} render={route.component} />
+          ))}
         </Switch>
       </BrowserRouter>
     </>
