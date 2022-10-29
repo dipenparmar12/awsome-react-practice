c547134641fbb5701919363d00ba8558d4a8486a
Dipen parmar <dipensavji@gmail.com>
2022-10-29 14:29:09 +0530

    feat(layout): 3 sections.

--- react-admin-dashboard/src/App.js
+++ react-admin-dashboard/src/App.js
@@ -1,12 +1,19 @@
-import BtTable from './components/example/BtTable'
-import 'bootstrap/scss/bootstrap.scss'
+/* eslint-disable prettier/prettier */
+
+// import 'bootstrap/scss/bootstrap.scss'
 import './styles/main.scss'
 
 function App() {
   return (
-    <>
-      <BtTable />
-    </>
+    <div className="main_container">
+      <aside className="navigation"> LEFT SIDE BAR </aside>
+      <main>
+        Main Content
+        {/* <div className="t1"> T1 </div>
+        <div className="t2"> T2 </div> */}
+      </main>
+      <aside className="navigation_right "> RIGHT PROFILE BAR </aside>
+    </div>
   )
 }
 
