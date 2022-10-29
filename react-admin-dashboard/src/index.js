c2531cdf8fccdab71f837932752f5ad229f69502
Dipen parmar <dipensavji@gmail.com>
2022-10-29 21:43:39 +0530

    feat(api-client): react-query

--- react-admin-dashboard/src/index.js
+++ react-admin-dashboard/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+
 import './index.scss'
+
+// Create a API client
+const queryClient = new QueryClient()
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 )
