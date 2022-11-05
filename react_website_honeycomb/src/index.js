767d096498af5af5ad307f630633e27279b276f5
Dipen parmar <dipensavji@gmail.com>
2022-11-05 09:59:53 +0530

    feat(task_website_honeycomb): Eslint.

--- react_website_honeycomb/src/index.js
+++ react_website_honeycomb/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
@@ -8,10 +8,10 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById('root'),
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
