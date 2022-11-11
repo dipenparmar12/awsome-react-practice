067c4c8d99a821e2fce3d226b87443c75e08a007
Dipen parmar <dipensavji@gmail.com>
2022-11-11 16:02:32 +0530

    feat(expo_tutorial): project init.

---
+++ expo_tutorial/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>
+        Testing app
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ expo_tutorial/Readme.md
@@ -0,0 +1,4 @@
+### Docs 
+
+ - `yarn global add expo-cli`: add expo-cli globally 
+ - `expo init ExpoApp`: Create new expo project 
