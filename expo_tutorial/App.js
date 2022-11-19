255e85dacbe7f70b351adba3548ea516991c1e6d
Dipen parmar <dipensavji@gmail.com>
2022-11-19 12:03:19 +0530

    feat(expo_tutorial): project init.

--- expo_tutorial/App.js
+++ expo_tutorial/App.js
@@ -1,15 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>
-        Testing app
-      </Text>
-      <StatusBar style="auto" />
+      <Text> One two </Text>
+      <StatusBar style='auto' />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -19,4 +17,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
--- expo_tutorial/Readme.md
+++ expo_tutorial/Readme.md
@@ -1,4 +1,8 @@
 ### Docs 
 
- - `yarn global add expo-cli`: add expo-cli globally 
- - `expo init ExpoApp`: Create new expo project 
+ - `yarn global add expo-cli`: installs the Expo command-line tools.
+ - `expo init ExpoApp`:  creates a new Expo project named "ExpoApp"
+ - `npx expo start --web` Run expo project in browser --web
+
+
+- If your device is on the same Wi-Fi network, set 'Debug server host & port for device' in 'Dev settings' to your machine's IP address and the port of the local dev server - e.g. `10.0.1.1:8081`
