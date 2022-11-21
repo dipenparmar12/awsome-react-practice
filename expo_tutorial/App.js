448155ba1ad8dceea50edf7a6d271c7229ad1615
Dipen parmar <dipensavji@gmail.com>
2022-11-21 18:26:12 +0530

    feat(expo):  challenge one

--- expo_tutorial/App.js
+++ expo_tutorial/App.js
@@ -1,5 +1,11 @@
 import AlertTest from './assets/Test/AlertTest'
+import Exercise1 from './assets/Test/Exercise1'
 
 export default function App() {
-  return <AlertTest />
+  return (
+    <>
+      <Exercise1 />
+      {/* <AlertTest /> */}
+    </>
+  )
 }
