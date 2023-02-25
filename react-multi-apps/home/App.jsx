8b5e68431ea81b08a9247b00a079d9ccd564a7c1
Dipen parmar <dipensavji@gmail.com>
2023-02-25 18:46:11 +0530

    feat(react/multi-apps): multi-pal apps using express-node route.

---
+++ react-multi-apps/home/App.jsx
@@ -0,0 +1,59 @@
+import { Routes, Route, Outlet, Link } from 'react-router-dom'
+import { NoMatch } from '../inbox/no-match'
+import './index.css'
+
+export default function HomeApp() {
+  return (
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='*' element={<NoMatch />} />
+      </Route>
+    </Routes>
+  )
+}
+
+function Layout() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+          <li>
+            {/* Use a normal <a> when linking to the "Inbox" app so the browser
+                does a full document reload, which is what we want when exiting
+                this app and entering another so we execute its entry point in
+                inbox/main.jsx. */}
+            <a href='/inbox'>Inbox</a>
+          </li>
+        </ul>
+      </nav>
+
+      <hr />
+
+      <Outlet />
+    </div>
+  )
+}
+
+function Home() {
+  return (
+    <div>
+      <p>This is the home page.</p>
+    </div>
+  )
+}
+
+function About() {
+  return (
+    <div>
+      <p>This is the about page.</p>
+    </div>
+  )
+}
