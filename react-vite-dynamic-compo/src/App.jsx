084a72dfa841ae6e0d030b525c28f8fa23b159df
Dipen parmar <dipensavji@gmail.com>
2023-03-19 16:10:00 +0530

    fix(react-vite-dynamic-compo): Eslint & Prettier

--- react-vite-dynamic-compo/src/App.jsx
+++ react-vite-dynamic-compo/src/App.jsx
@@ -1,11 +1,11 @@
-import ResultsSummaryComponent from './challanges/ResultsSummaryComponent';
+import ResultsSummaryComponent from './challanges/ResultsSummaryComponent'
 
 function App() {
   return (
     <div className="App">
       <ResultsSummaryComponent />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
