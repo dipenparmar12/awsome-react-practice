70613e09462ce27613db45f864c22d6f8b429c43
Dipen parmar <dipensavji@gmail.com>
2023-03-19 14:52:46 +0530

    feat(react-vite-starter): init SASS and Tailwind

---
+++ react-vite/readme.md
@@ -0,0 +1,16 @@
+### vite react 
+
+
+## Dependenceis 
+
+Sass 
+> `pnpm add sass`
+
+TailwindCss 
+> `pnpm add -D autoprefixer postcss tailwindcss`
+
+EsLint 
+> `pnpm add -D eslint eslint-config-airbnb eslint-import-resolver-alias eslint-plugin-react eslint-plugin-react-hooks`
+
+EsLint + Prettier
+> `pnpm add -D  eslint-config-prettier eslint-plugin-prettier`
--- react-vite/src/App.jsx
+++ react-vite/src/App.jsx
@@ -1,7 +1,11 @@
-import { useState } from 'react'
+import ResultsSummaryComponent from './challanges/ResultsSummaryComponent'
 
 function App() {
-  return <div className='App'>React starter</div>
+  return <div className='App'>
+    <h1 className="text-3xl !text-blue-400 font-bold underline text-center">Hello world!</h1> 
+
+    <ResultsSummaryComponent/>
+  </div>
 }
 
 export default App
