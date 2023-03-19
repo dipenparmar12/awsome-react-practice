2b11f6396f9e6c81e85d83ce74b05277beb5417b
Dipen parmar <dipensavji@gmail.com>
2023-03-19 15:43:17 +0530

    feat(react-vite-dynamic-compo): init

---
+++ react-vite-dynamic-compo/src/App.jsx
@@ -0,0 +1,11 @@
+import ResultsSummaryComponent from './challanges/ResultsSummaryComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <ResultsSummaryComponent />
+    </div>
+  );
+}
+
+export default App;
